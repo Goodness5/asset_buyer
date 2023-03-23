e1cccc431e7ad42b5d2689e1cebb748c7c00be3c
Goodness5 <goodnesskolapo@gmail.com>
2023-03-23 01:26:30 +0100

    damn

--- contracts/libraries/libAssetData.sol
+++ contracts/libraries/libAssetData.sol
@@ -2,6 +2,14 @@
 pragma solidity ^0.8.17;
 
 
+struct Assetdetails {
+    string AssetName;
+    uint256 AssetPrice;
+    address Assetowner;
+    address AssetAddress;
+    bool Assetpurchased;
+}
+
 struct AssetData {
-    string Assetname;
+    mapping (uint256 => Assetdetails) Assetdetail;
 }
