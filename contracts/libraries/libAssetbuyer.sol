09f17104a6b6757ae921b6055fd46efb01e1f805
Goodness5 <goodnesskolapo@gmail.com>
2023-03-23 04:39:45 +0100

    omo

--- contracts/libraries/libAssetbuyer.sol
+++ contracts/libraries/libAssetbuyer.sol
@@ -7,6 +7,8 @@ import "../../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3
 import "../../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol/";
 
 library libAssetbuyer {
+
+
     function _stageAsset(
         string memory assetName,
         uint256 assetPrice,
@@ -29,48 +31,6 @@ library libAssetbuyer {
     }
 
 
-    function addpricefeeed(string memory paymentname, AggregatorV3Interface pricefeedAdddess) internal {
-        AssetData storage ds = AssetSlot();
-
-        ds.paymentmethods.push(paymentname);
-        ds.paymentmethod[paymentname].paymentToken = pricefeedAdddess;
-       AggregatorV3Interface pricefeed = AggregatorV3Interface(pricefeedAdddess);
-        (, int Price, , , ) = getLatestPrice(pricefeed);
-        Price = ds.paymentmethod[paymentname].price;
-
-    }
-   function getprice(string memory _paymentname) internal view returns (int _price) {
-    AssetData storage ds = AssetSlot();
-    string[] memory names = ds.paymentmethods;
-    for (uint i = 0; i < names.length; i++) {
-         string memory name = names[i];
-        if (keccak256(bytes(name))==keccak256(bytes(_paymentname))) {
-            _price = ds.paymentmethod[_paymentname].price;
-        }else{
-            revert("payment method not found");
-        }
-    }
-        return _price;
-}
-
-
-      function getAssetDetails(uint256 assetId) public view returns (Assetdetails memory) {
-        AssetData storage assetData = AssetSlot();
-        require(assetData.Assetdetail[assetId].AssetAddress != address(0), "Asset does not exist");
-        return assetData.Assetdetail[assetId];
-    }
-
-  function isAssetPurchased(uint256 assetId) public view returns (bool) {
-        AssetData storage assetData = AssetSlot();
-        require(assetData.Assetdetail[assetId].AssetAddress != address(0), "Asset does not exist");
-        return assetData.Assetdetail[assetId].Assetpurchased;
-    }
-
-
-    function getLatestPrice (AggregatorV3Interface pricefeed) public view returns (uint80 roundID, int price,
-        uint  startedAt,uint timeStamp,uint80 answeredInRound) {
-        (roundID, price,startedAt,timeStamp,answeredInRound) = pricefeed.latestRoundData();
-    } 
 
     function AssetSlot() internal pure returns(AssetData storage ds) {
         assembly {
